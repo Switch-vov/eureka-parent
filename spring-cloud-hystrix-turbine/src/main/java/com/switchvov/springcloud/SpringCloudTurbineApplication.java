e20c82df662a3f3f63c7470b5c94fe0a67580e14
Switch <q547550831@outlook.com>
2017-08-27 21:11:06 +0800

    turbine配置应用集群监控

---
+++ spring-cloud-hystrix-turbine/src/main/java/com/switchvov/springcloud/SpringCloudTurbineApplication.java
@@ -0,0 +1,16 @@
+package com.switchvov.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableTurbine
+public class SpringCloudTurbineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudTurbineApplication.class, args);
+    }
+}
