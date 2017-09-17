0341888905214c9d0ceaea627cc4e2789a221f22
Switch <q547550831@outlook.com>
2017-09-17 10:09:15 +0800

    使用Docker镜像打包eureka-server-docker项目

--- eureka-server-docker/src/main/resources/docker-dir/script.sh
+++ eureka-server-docker/src/main/resources/docker-dir/script.sh
@@ -2,3 +2,10 @@
 docker build -t switchvov/eureka-server-docker:0.0.1 .
 # 运行Docker镜像
 docker run -d -p 91:91 switchvov/eureka-server-docker:0.0.1
+
+# 登录Docker Hub
+docker login
+# 推送镜像(先在Docker Hub对应账号下，增加相应仓库)
+docker push switchvov/eureka-server-docker:0.0.1
+# 搭建私有仓库
+docker run -d -p 5000:5000 --restart=always --name registry2 registry:2
